6b847a789e410992abc4b9b83b92a8c2748544da
Steve Griegoliet <steven.griegoliet@visanow.com>
2017-06-29 15:00:50 -0500

    Adding .bin to path?

--- deploy.cmd
+++ deploy.cmd
@@ -109,7 +109,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 
 echo Create Hubot file with a coffee extension
 :: 4. Create Hubot file with a coffee extension
-copy /Y "%DEPLOYMENT_TARGET%\node_modules\hubot\bin\hubot" "%DEPLOYMENT_TARGET%\node_modules\hubot\bin\hubot.coffee"
+copy /Y "%DEPLOYMENT_TARGET%\node_modules\hubot\.bin\hubot" "%DEPLOYMENT_TARGET%\node_modules\hubot\.bin\hubot.coffee"
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
